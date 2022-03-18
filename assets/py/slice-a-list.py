7ae686081468d08fd6de93317835699ed286a436
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-03-18 19:05:04 +0000

    Test slice method on a list

---
+++ assets/py/slice-a-list.py
@@ -0,0 +1,6 @@
+
+list_of_strings = ['Barry', 'Tim', 'Tony', 'Michelle', 'Anne', 'Margret']
+
+# Creates a slice object.
+new_list = slice(5)
+print(list_of_strings[new_list]) # Prints the new list 
