616f163bb25b7cfdc441da5f1c062146005e232f
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-03-19 09:57:43 +0000

    Add loops for iterating dictionaries and lists

---
+++ assets/py/iteration.py
@@ -0,0 +1,22 @@
+data = {
+	"first_name": "brian",
+	"last_name": "johnson",
+	"occupation": "student"
+}
+
+scores = [6, 9, 8, 7, 8, 9]
+
+# iteration with key value pairs using the items method.
+for key, value in data.items():
+    if value is not 'student':
+        data[key] = value.capitalize()
+        
+
+print(data)
+
+# iteration using the range and len functions 
+for i in range(len(scores)):
+    scores[i]+=1
+
+
+print(scores)
